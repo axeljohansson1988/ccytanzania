eaca4066b17c2beb2c2d4f2cdf7785fee02d2398
axeljohansson1988 <axeljohansson88@gmail.com>
2016-10-24 19:43:05 +0200

    no message

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'Jupiter123');
 
 /** MySQL hostname */
-define('DB_HOST', 'http://bt25-php-dev.cloudapp.net/');
+define('DB_HOST', 'bt25-php-dev.cloudapp.net/');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
